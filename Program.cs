6870444e2219d1ee7c654d3e2fd5aa399d4d3acf
Piapedroso <mariapiapedroso@gmail.com>
2024-04-22 22:37:18 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Bienvenidos");
+string nombre;
+
+nombre = "Pia";
+
+const int dni = 5555555;
+
+
+Console.WriteLine("Mi nombre es:" + nombre);
+Console.WriteLine("Mi dni es:" + dni);
